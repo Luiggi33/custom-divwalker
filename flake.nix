1b81a063fb50650cc20877db50de78502145d747
philippschuetz <mail@philippschuetz.com>
2025-05-08 18:10:03 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
-  description = "Java development environment";
+  description = "NodeJS development environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {nixpkgs}: let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
       packages = with pkgs; [
-        nodejs_23
+        nodejs_24
       ];
 
       shellHook = ''
